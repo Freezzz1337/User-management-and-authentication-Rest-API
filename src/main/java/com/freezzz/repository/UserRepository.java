55f4786be65ba2434b0950a701cc65a635b73cbd
Artem Samokysh <samokish777bombila@gmail.com>
2023-06-12 18:01:08 +0300

    create UserRepository

---
+++ src/main/java/com/freezzz/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.freezzz.repository;
+
+import com.freezzz.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByLogin(String login);
+}
