ffc10ddf44a5aeab58681c27154cdcfbe7f9e357
Artem Samokysh <samokish777bombila@gmail.com>
2023-06-13 18:44:22 +0300

    create JwtService and do some changes in JwtAuthenticationFilter

---
+++ src/main/java/com/freezzz/config/JwtService.java
@@ -0,0 +1,10 @@
+package com.freezzz.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public String extractUserLogin(String token) {
+        return null;
+    }
+}
